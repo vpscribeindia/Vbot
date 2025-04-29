d6b55ac01d7a7692be4703ac2516f6513fb4063c
JerinRijo <206755238+ajishjerinrijo@users.noreply.github.com>
2025-04-30 00:05:33 +0530

    bill model

--- server/database-service/models/billing.js
+++ server/database-service/models/billing.js
@@ -8,18 +8,26 @@ const Billing = sequelize.define("Billing", {
         references: { model: 'Users', key: 'id' }
 
     },
+    paypal_order_id: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
     amount: {
         type: DataTypes.FLOAT,
         allowNull: false,
     },
-    status: {
+    payment_status: {
         type: DataTypes.ENUM("pending", "paid", "failed"),
         defaultValue: "pending",
     },
+    status: {
+        type: DataTypes.ENUM("active", "expired", "cancelled"),
+        defaultValue: "active",
+    },
     pakage_type: {
-        type: DataTypes.ENUM("basic", "standard", "premium"),
+        type: DataTypes.ENUM("basic", "standard", "premium", "trial"),
         allowNull: false,
-        defaultValue: "basic",
+        defaultValue: "trial",
     },
     package_end_date: {
         type: DataTypes.DATE,
@@ -37,6 +45,7 @@ const Billing = sequelize.define("Billing", {
         type: DataTypes.STRING,
         allowNull: false,
     },
+
 });
 
 module.exports = Billing;
