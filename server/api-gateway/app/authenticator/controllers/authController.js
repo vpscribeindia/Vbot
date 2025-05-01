a007728b9c51898fb3f0053c419655475110d2df
JerinRijo <206755238+ajishjerinrijo@users.noreply.github.com>
2025-05-02 01:09:44 +0530

    login role base-jerin

--- server/api-gateway/app/authenticator/controllers/authController.js
+++ server/api-gateway/app/authenticator/controllers/authController.js
@@ -154,7 +154,7 @@ const login = async (req, res) => {
           return res.status(401).json({ message: "Invalid credentials" });
       }
 
-const token = jwt.sign({id: user.id }, process.env.JWT_SECRET, {
+const token = jwt.sign({id: user.id, role:user.role}, process.env.JWT_SECRET, {
 expiresIn: '24h',
 });
 
